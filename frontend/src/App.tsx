75256f6f7fb60a98078f84105e6bb4600d375797
Victor Polyakov <polyakov.victor21@gmail.com>
2024-08-19 00:20:05 +0300

    init frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import { Component, useState } from 'react'
+import Header from "./Components/Header"
+import './index.css'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <div className='extend'>
+        
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/Components/Header/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import s from "./Style/Header.module.css"
+
+const Header = () => {
+    return (
+        <div className={s.root}>
+            <div className={s.leftTopic}>
+                <h1>left Header</h1>
+            </div>
+            <div className={s.rightTopic}>
+                <h1>right Header</h1>
+            </div>
+        </div>
+    )   
+}
+
+export default Header;
